fe5ce32c59d9378a5ee1036c2793581b40832f38
Maria Andreza C Santos <mandreza008@gmail.com>
2026-08-30 13:10:29 -0300

    feat: adiciona a chamada do conversor no main

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 
 from fastapi import FastAPI
 
-from src.csv_to_xlsx.api.routes import router
+from src.csv_to_xlsx.api.routes import router as csv_to_xlsx_router
+from src.json_to_csv.api.routes import router as json_to_csv_router
 
 app = FastAPI(title="File Converter API")
 
-app.include_router(router)
+app.include_router(csv_to_xlsx_router)
+app.include_router(json_to_csv_router)
