b85427166a8f3564f423f2b315ec4e8e4155ef4f
Maria Andreza C Santos <mandreza008@gmail.com>
2026-09-01 15:39:10 -0300

    feat: adiciona conversores no main

--- main.py
+++ main.py
@@ -9,9 +9,13 @@ from src.csv_to_xml.api.routes import router as csv_to_xml_router
 from src.json_to_csv.api.routes import router as json_to_csv_router
 from src.json_to_xlsx.api.routes import router as json_to_xlsx_router
 from src.json_to_xml.api.routes import router as json_to_xml_router
+from src.pdf_to_word.api.routes import router as pdf_to_word_router
+from src.pdf_to_xlsx.api.routes import router as pdf_to_xlsx_router
 from src.txt_to_csv.api.routes import router as txt_to_csv_router
+from src.word_to_pdf.api.routes import router as word_to_pdf_router
 from src.xlsx_to_csv.api.routes import router as xlsx_to_csv_router
 from src.xlsx_to_json.api.routes import router as xlsx_to_json_router
+from src.xlsx_to_pdf.api.routes import router as xlsx_to_pdf_router
 from src.xlsx_to_xml.api.routes import router as xlsx_to_xml_router
 from src.xml_to_csv.api.routes import router as xml_to_csv_router
 from src.xml_to_json.api.routes import router as xml_to_json_router
@@ -25,9 +29,13 @@ app.include_router(csv_to_xml_router)
 app.include_router(json_to_csv_router)
 app.include_router(json_to_xlsx_router)
 app.include_router(json_to_xml_router)
+app.include_router(pdf_to_word_router)
+app.include_router(pdf_to_xlsx_router)
 app.include_router(txt_to_csv_router)
+app.include_router(word_to_pdf_router)
 app.include_router(xlsx_to_csv_router)
 app.include_router(xlsx_to_json_router)
+app.include_router(xlsx_to_pdf_router)
 app.include_router(xlsx_to_xml_router)
 app.include_router(xml_to_csv_router)
 app.include_router(xml_to_json_router)
