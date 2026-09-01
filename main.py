9558c56e89b13f41eddd7d4935e6247328a90b70
Maria Andreza C Santos <mandreza008@gmail.com>
2026-09-01 14:16:52 -0300

    feat: adiciona conversores no main

--- main.py
+++ main.py
@@ -8,9 +8,13 @@ from src.csv_to_xlsx.api.routes import router as csv_to_xlsx_router
 from src.csv_to_xml.api.routes import router as csv_to_xml_router
 from src.json_to_csv.api.routes import router as json_to_csv_router
 from src.json_to_xlsx.api.routes import router as json_to_xlsx_router
+from src.json_to_xml.api.routes import router as json_to_xml_router
+from src.txt_to_csv.api.routes import router as txt_to_csv_router
 from src.xlsx_to_csv.api.routes import router as xlsx_to_csv_router
 from src.xlsx_to_json.api.routes import router as xlsx_to_json_router
+from src.xlsx_to_xml.api.routes import router as xlsx_to_xml_router
 from src.xml_to_csv.api.routes import router as xml_to_csv_router
+from src.xml_to_json.api.routes import router as xml_to_json_router
 from src.xml_to_xlsx.api.routes import router as xml_to_xlsx_router
 
 app = FastAPI(title="File Converter API")
@@ -20,8 +24,12 @@ app.include_router(csv_to_xlsx_router)
 app.include_router(csv_to_xml_router)
 app.include_router(json_to_csv_router)
 app.include_router(json_to_xlsx_router)
+app.include_router(json_to_xml_router)
+app.include_router(txt_to_csv_router)
 app.include_router(xlsx_to_csv_router)
 app.include_router(xlsx_to_json_router)
+app.include_router(xlsx_to_xml_router)
 app.include_router(xml_to_csv_router)
+app.include_router(xml_to_json_router)
 app.include_router(xml_to_xlsx_router)
 app.include_router(core_router)
